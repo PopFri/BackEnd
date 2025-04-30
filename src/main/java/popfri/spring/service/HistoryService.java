26a736948229b7360337b53db81e71ca4adfbd64
ohige01 <ohige01@naver.com>
2025-04-30 19:23:03 +0900

    [FEAT] #30 유저 방문 기록 조회 구현

--- src/main/java/popfri/spring/service/HistoryService.java
+++ src/main/java/popfri/spring/service/HistoryService.java
@@ -81,4 +81,8 @@ public class HistoryService {
                     .build());
         }
     }
+
+    public List<VisitHistory> getVisitHistory(User user){
+        return visitHistoryRepository.findByUserOrderByUpdatedAtDesc(user);
+    }
 }
